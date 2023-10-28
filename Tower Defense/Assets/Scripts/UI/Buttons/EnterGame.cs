ed5308139e9a41c0359b3ee1538b9137dde333f3
Gino Schaap <35615@ma-web.nl>
2023-10-28 15:49:41 +0200

    compiler error fixing

--- Tower Defense/Assets/Scripts/UI/Buttons/EnterGame.cs
+++ Tower Defense/Assets/Scripts/UI/Buttons/EnterGame.cs
@@ -1,9 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using UnityEngine.SceneManagement;
-using UnityEditor.SceneManagement;
 using UnityEngine.SceneManagement;
+
 
 public class EnterGame : MonoBehaviour
 {
